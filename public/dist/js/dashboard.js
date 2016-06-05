da2ab5c1add1fdd01d794c635626db2e240b3ef2
esauOp <monkeycoss@gmail.com>
2016-06-04 19:29:54 -0500

    Erasing unusefull info.

--- public/dist/js/dashboard.js
+++ public/dist/js/dashboard.js
@@ -6,102 +6,9 @@ $(function(){
 	socket.on('comm', function (data) {
 		console.log(data);
 	});
-	
-    var lineChartData = {
-        labels : ["January","February","March","April","May","June","July"],
-        datasets_Y1 : [
-            {
-                fillColor : "rgba(220,220,220,0.5)",
-                strokeColor : "rgba(220,220,220,1)",
-                pointColor : "rgba(220,220,220,1)",
-                pointStrokeColor : "#fff",
-                data : [15,25,90,81,56,55,400]
-            }
-        ],
-        datasets_Y2 : [
-            {
-                fillColor : "rgba(151,187,205,0.5)",
-                strokeColor : "rgba(151,187,205,1)",
-                pointColor : "rgba(151,187,205,1)",
-                pointStrokeColor : "#fff",
-                data : [105,48,40,19,96,27,100]
-            }
-        ],
-        datasetss: [
-      	{
-          label: "Temperature",
-          fillColor: "rgba(243, 156, 18, 0.2)",
-          strokeColor: "rgba(210, 214, 222, 1)",
-          pointColor: "rgba(243, 156, 18, 1)",
-          pointStrokeColor: "#c1c7d1",
-          pointHighlightFill: "#fff",
-          pointHighlightStroke: "rgba(220,220,220,1)",
-          data : [105,48,40,19,96,27,100]
-        }
-      	]
-    };
 
-	var tempChartData = {
-      	labels : ["January","February","March","April","May","June","July"],
-      	datasets: [
-      	{
-          label: "Temperature",
-          fillColor: "rgba(243, 156, 18, 0.2)",
-          strokeColor: "rgba(210, 214, 222, 1)",
-          pointColor: "rgba(243, 156, 18, 1)",
-          pointStrokeColor: "#c1c7d1",
-          pointHighlightFill: "#fff",
-          pointHighlightStroke: "rgba(220,220,220,1)",
-          data : [105,48,40,19,96,27,100]
-        }
-      	]
-  	};
-
-    var areaChartOptions = {
-			//Boolean - If we should show the scale at all
-			showScale: true,
-			//Boolean - Whether grid lines are shown across the chart
-			scaleShowGridLines: false,
-			//String - Colour of the grid lines
-			scaleGridLineColor: "rgba(0,0,0,.05)",
-			//Number - Width of the grid lines
-			scaleGridLineWidth: 1,
-			//Boolean - Whether to show horizontal lines (except X axis)
-			scaleShowHorizontalLines: true,
-			//Boolean - Whether to show vertical lines (except Y axis)
-			scaleShowVerticalLines: true,
-			//Boolean - Whether the line is curved between points
-			bezierCurve: true,
-			//Number - Tension of the bezier curve between points
-			bezierCurveTension: 0.3,
-			//Boolean - Whether to show a dot for each point
-			pointDot: false,
-			//Number - Radius of each point dot in pixels
-			pointDotRadius: 4,
-			//Number - Pixel width of point dot stroke
-			pointDotStrokeWidth: 1,
-			//Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-			pointHitDetectionRadius: 20,
-			//Boolean - Whether to show a stroke for datasets
-			datasetStroke: true,
-			//Number - Pixel width of dataset stroke
-			datasetStrokeWidth: 2,
-			//Boolean - Whether to fill the dataset with a color
-			datasetFill: true,
-			//String - A legend template
-			legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].lineColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
-			//Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-			maintainAspectRatio: true,
-			//Boolean - whether to make the chart responsive to window resizing
-			responsive: true,
-
-			pointDot: true,
-			scaleLabel: "<%=value%>*",
-			animation : false
-
-		};
-
-    var areaChartCanvas = $("#areaChart").get(0).getContext("2d");
+    // var areaChartCanvas = $("#areaChart").get(0).getContext("2d");
+    var humidityChartCanvas = $("#humidityChart").get(0).getContext("2d");
 
     Chart.types.Line.extend({
 	    name: "Line2Y",
@@ -199,8 +106,46 @@ $(function(){
 	    }]
 	}
 
-	var ctx = document.getElementById("areaChart").getContext("2d");
-	var myLine1 = new Chart(ctx).Line2Y(lineChartData1, {
+	var lineChartData_2 = {
+	    labels: ["18:55", "18:56", "18:57", "18:58", "18:59", "19:00", "19:01"],
+	    datasets: [{
+	        label: "My First dataset",
+	        fillColor: "rgba(151,187,205,0.5)",
+	        strokeColor: "rgba(151,187,205,1)",
+	        pointColor: "rgba(151,187,205,1)",
+	        pointStrokeColor: "#fff",
+	        data: [90, 91, 95, 100, 110, 115, 120]
+	    }]
+	}
+
+	var lineChartData_3 = {
+	    labels: ["18:55", "18:56", "18:57", "18:58", "18:59", "19:00", "19:01"],
+	    datasets: [{
+	        label: "My First dataset",
+	        fillColor : "rgba(220,220,220,0.5)",
+            strokeColor : "rgba(220,220,220,1)",
+            pointColor : "rgba(220,220,220,1)",
+            pointStrokeColor : "#fff",
+	        data: [40, 51, 65, 100, 110, 115, 120]
+	    }]
+	}
+
+	// var ctx = document.getElementById("areaChart").getContext("2d");
+	var humidityChartEl = document.getElementById("humidityChart").getContext("2d");
+	var moiChartEl = document.getElementById("moiChart").getContext("2d");
+
+	// var myLine1 = new Chart(ctx).Line2Y(lineChartData1, {
+	//     scaleBeginAtZero: true,
+	//     scaleShowGridLines: false
+	// });
+
+	var myLine2 = new Chart(humidityChartEl).Line(lineChartData_2, {
+	    scaleBeginAtZero: true,
+	    scaleShowGridLines: false
+	});
+
+
+	var myLine3 = new Chart(moiChartEl).Line(lineChartData_3, {
 	    scaleBeginAtZero: true,
 	    scaleShowGridLines: false
 	});
